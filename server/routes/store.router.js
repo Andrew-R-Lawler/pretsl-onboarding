e2a922db88dc7e8b95f833d9c481662fb68d2087
Andrew Lawler <andrewlawler@Andrews-MacBook-Air.local>
2020-07-26 12:25:09 -0500

    admin dashboard GETs stores from AWS database, stores array in storeReducer

--- server/routes/store.router.js
+++ server/routes/store.router.js
@@ -6,7 +6,14 @@ const router = express.Router();
  * GET route for store
  */
 router.get('/', (req, res) => {
-
+    pool.query('SELECT * FROM "store";')
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('Error GET /store failed,', error);
+            res.sendStatus(500)
+        })
 });
 
 /**
