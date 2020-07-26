b60abb75b23b2a099fed681d6fcfc99ed349d55e
Steve Simonson <srsimonson@gmail.com>
2020-07-26 11:11:06 -0500

    componentDidMount, table heads for admin dashboard

--- src/components/AdminDashboard/AdminDashboard.js
+++ src/components/AdminDashboard/AdminDashboard.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+
 class AdminDashboard extends Component {
+
+    componentDidMount = () => { 
+        this.props.dispatch ({ type: 'GET_STORES' })
+      };
+
     render(){
         return(
             <div>
                 <h1>Admin Dashboard</h1>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Actions</th>
+                                <th>Store Name</th>
+                                <th>Email</th>
+                                <th>Status</th>
+                                <th>Date Joined</th>
+                                <th>Notes</th>
+                                <th>Contract</th>
+                                <th>Business Type</th>
+                                <th>Customer Status</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
         )
     }
