49766c27a21e01133ca7cea8c3732cda75c798b8
Jake Dahl <jakedahl16@yahoo.com>
2020-07-28 10:20:39 -0500

    merge

--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Locations extends Component {
-    customerLocation = () => {
-        event.preventDefault();
+    componentDidMount = () => {
         this.props.dispatch({ type: 'GET_LOCATIONS' })
     }
     render(){
-        return(
-            <div>
-                <h1>Locations</h1>
-            </div>
-        )
+        const location = this.props.reduxState.locationsReducer;
+        return (
+          <div>
+            <h1>Locations</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Store Name</th>
+                  <th>Request Type</th>
+                  <th>Description</th>
+                  <th>Ticket Number</th>
+                  <th>Date Requested</th>
+                  <th>Status</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              {location.map((item) => (
+                <tr key={item.id}>
+                  {/* <td>{item.store_id}</td> */}
+                  {/* <td>{item.request_type}</td> */}
+                  {/* <td>{item.request_body}</td> */}
+
+                  <td>{item.address}</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        );
     }
 }
 
