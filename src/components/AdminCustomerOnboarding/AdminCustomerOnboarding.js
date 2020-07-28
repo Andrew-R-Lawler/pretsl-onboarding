6ab19d688758a54fa566bfd1a0822b006447e74b
Andrew Lawler <andrewlawler@Andrews-MacBook-Air.local>
2020-07-28 11:00:42 -0500

    basic JSX frame for onboarding form

--- src/components/AdminCustomerOnboarding/AdminCustomerOnboarding.js
+++ src/components/AdminCustomerOnboarding/AdminCustomerOnboarding.js
@@ -6,6 +6,22 @@ class CustomerOnboarding extends Component {
         return (
             <div>
                 <h1>Customer Onboarding</h1>
+                <h3>Store Name</h3>
+                <input placeholder = 'Store Name'></input>
+                <h3>Store Status</h3>
+                <input placeholder = 'Store Status'></input>
+                <h3>Date Joined</h3>
+                <input placeholder = 'Date Joined'></input>
+                <h3>Notes</h3>
+                <textarea placeholder = 'Notes'></textarea>
+                <h3>Contract</h3>
+                <h3>Business Type</h3>
+                <input placholder = 'Business Type'></input>
+                <h3>MoonClerk URL</h3>
+                <input placeholder = 'MoonClerk URL'></input>
+                <h3>Customer Email</h3>
+                <input placeholder = 'Customer Email'></input>
+                <button>Submit</button>
             </div>
         )
     }
