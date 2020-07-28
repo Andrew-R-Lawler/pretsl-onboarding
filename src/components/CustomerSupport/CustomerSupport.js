a3f89054a43b313ff34eb333f17aec7ad91500f0
Steve Simonson <srsimonson@gmail.com>
2020-07-27 23:12:20 -0500

    customer support almost complete. needs join on sqlText in support.router

--- src/components/CustomerSupport/CustomerSupport.js
+++ src/components/CustomerSupport/CustomerSupport.js
@@ -3,20 +3,35 @@ import { connect } from 'react-redux';
 
 class CustomerSupport extends Component {
 
+    state = {
+        requestType: '',
+        requestBody: '',
+        requestStatus: 'New Request'
+    }
+
+    handleChange = (event) => {
+        // console.log('event.target.value', event.target.value);
+        this.setState({
+            // ...this.state,
+            [event.target.name]: event.target.value
+        })
+        // console.log('this.state', this.state);
+    }
+
     submit = () => {
-        console.log('submit clicked');
-        
+        this.props.dispatch({
+            type: 'CREATE_NEW_TICKET',
+            payload: this.state
+        })
     }
 
     render() {
-        console.log('this.props.reduxStore.user', this.props.reduxStore.user);
         return (
             <div>
                 <div>
                     <h1>Customer Support</h1>
-                    <h2>Thank you for reaching out.</h2>
-                    <p>Copy here about making a support request. Please fill out the form. Someone will respond to the email associated with the account. </p>
-                    <p>From Purple Onion: 'We’re here to help make the planning of your event easier. Whether you’ve got questions about placing an order, creating a menu for your event or just need help getting started, we’re here to help. Fill out the form here and someone will be in touch with you soon.'</p>
+                        <h2>Thank you for reaching out.</h2>
+                            <p>From Purple Onion: 'We’re here to help make the planning of your event easier. Whether you’ve got questions about placing an order, creating a menu for your event or just need help getting started, we’re here to help. Fill out the form here and someone will be in touch with you soon.'</p>
                 </div>
                 <form>
                     <div>
@@ -26,15 +41,17 @@ class CustomerSupport extends Component {
                         <p>If there is a better name or contact info to address this request, please specify so in the details section below.</p>
                     </div>
                     <label>Request type:</label>
-                    <select>
+                    <select name="requestType" onChange={this.handleChange}>
+                        <option value="" disabled selected value>Choose</option>
                         <option value="Change Contact Info">Change Contact Info</option>
                         <option value="Change Payment Information">Change Payment Information</option>
                         <option value="Change Plan">Change Plan</option>
                         <option value="Cancel Pretsl Account">Cancel Pretsl Account</option>
                     </select>
                     <br/>
+                    <textarea name="requestBody" placeholder="How can we help you?" onChange={this.handleChange}></textarea>
+                    <br/>
                     <button onClick={this.submit}>Submit</button>
-                    
                 </form>
             </div>
         )
