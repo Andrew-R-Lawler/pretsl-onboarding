92e7922102e9c632344c2be6d56aa264b7b7cc0f
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-07-29 10:26:35 -0500

    Formated dates on admin support ticket

--- src/components/AdminSupport/AdminSupport.js
+++ src/components/AdminSupport/AdminSupport.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AdminSupportTicket from './AdminSupportTicket';
 
+
 class AdminSupport extends Component {
 
     componentDidMount = () => { 
