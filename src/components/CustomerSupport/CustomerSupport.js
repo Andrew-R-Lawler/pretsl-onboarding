79123aef49a1a2648f41f5bab85562a343dee852
Steve Simonson <srsimonson@gmail.com>
2020-07-30 23:48:32 -0500

    progress on customer support styling

--- src/components/CustomerSupport/CustomerSupport.js
+++ src/components/CustomerSupport/CustomerSupport.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './CustomerSupport.scss'
 
 class CustomerSupport extends Component {
 
@@ -38,24 +39,26 @@ class CustomerSupport extends Component {
                             <p>From Purple Onion: 'We’re here to help make the planning of your event easier. Whether you’ve got questions about placing an order, creating a menu for your event or just need help getting started, we’re here to help. Fill out the form here and someone will be in touch with you soon.'</p>
                 </div>
                 <form>
-                    <div>
+                    <div className="customer-support-instructions">
                         Name: {this.props.reduxStore.user.username}
                         <br/>
                         Contact Info: {this.props.reduxStore.user.user_email}
                         <p>If there is a better name or contact info to address this request, please specify so in the details section below.</p>
                     </div>
-                    <label>Request type:</label>
-                    <select name="requestType" onChange={this.handleChange}>
-                        <option value="" disabled selected value>Choose</option>
-                        <option value="Change Contact Info">Change Contact Info</option>
-                        <option value="Change Payment Information">Change Payment Information</option>
-                        <option value="Change Plan">Change Plan</option>
-                        <option value="Cancel Pretsl Account">Cancel Pretsl Account</option>
-                    </select>
-                    <br/>
-                    <textarea name="requestBody" placeholder="How can we help you?" onChange={this.handleChange}></textarea>
-                    <br/>
-                    <button onClick={this.submit}>Submit</button>
+                    <div className="customer-support-inputs">
+                        <label>Request type:</label>
+                        <select name="requestType" onChange={this.handleChange}>
+                            <option value="" disabled selected value>Choose</option>
+                            <option value="Change Contact Info">Change Contact Info</option>
+                            <option value="Change Payment Information">Change Payment Information</option>
+                            <option value="Change Plan">Change Plan</option>
+                            <option value="Cancel Pretsl Account">Cancel Pretsl Account</option>
+                        </select>
+                        <br/>
+                        <textarea name="requestBody" placeholder="How can we help you?" onChange={this.handleChange}></textarea>
+                        <br/>
+                        <button onClick={this.submit}>Submit</button>
+                    </div>
                 </form>
             </div>
         )
