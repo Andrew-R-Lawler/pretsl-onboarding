a8441fa3c994de3bd26e57c1dce96feb9ea30a23
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-08-04 16:10:47 -0500

    Files uploading to s3

--- server/server.js
+++ server/server.js
@@ -51,27 +51,84 @@ app.listen(PORT, () => {
 
 
 //aws bucket
-const aws = require('aws-sdk')
+// const aws = require('aws-sdk')
 
+// const s3 = new aws.S3();
+
+// app.post('/upload-url', async(req, res)=>{
+//   let expires = 900
+//   console.log("Inside AWS post call server side")
+
+//   s3.createPresignedPost({
+//     Bucket: 'pretslonboardingappbucket',
+//     Expires: expires,
+//     Fields:{
+//       key: 'test-file.txt'
+//     }
+//   },(err,data) =>{
+//     if(err){
+//       console.error(err);
+//       res.send(500);
+//       return;
+//     }
+//     res.send(data);
+//   })
+// });
+
+const multer = require('multer');
+const aws = require('aws-sdk');
+const fs = require('fs');
 const s3 = new aws.S3();
+const pool = require('./modules/pool');
 
-app.post('/upload-url', async(req, res)=>{
-  let expires = 900
-  console.log("Inside AWS post call server side")
 
-  s3.createPresignedPost({
-    Bucket: 'pretslonboardingappbucket',
-    Expires: expires,
-    Fields:{
-      key: 'test-file.txt'
-    }
-  },(err,data) =>{
-    if(err){
-      console.error(err);
-      res.send(500);
-      return;
-    }
-    res.send(data);
-  })
+const upload = multer({
+  storage: multer.diskStorage({
+
+  //upload files to public/uploads
+  destination: (req, file, cb) =>{
+    let uploadDirectory = 'public/uploads/'
+    cb(null, uploadDirectory);
+  },
+  
+
+//give each uploaded file a unique name by date 
+// so we don't overwrite files
+filename: (req, file, cb) => {
+  const filename = `${Date.now()}-${file.originalname}`;
+  cb(null, filename);
+}
+})
 });
 
+app.post('/upload', upload.any(), async(req, res) =>{
+  //Upload files
+  //note the upload.any() middleware
+
+  //multer loads everything to public/uploads
+  //then creates a req.files array with infor about each file.
+  //console.log(req.files);
+
+  for (let file of req.files){
+    console.log(`Uploading ${file.path}...`);
+
+    await s3.upload({
+
+      Bucket: 'pretslonboardingappbucket',
+      Key: file.filename,
+      Body: fs.createReadStream(file.path)
+    }).promise();  //promise is required to use await keyword
+    console.log(`Uploading ${file.path}...done.`);
+
+    await pool.query(`
+    INSERT INTO contracts (client_id, s3_bucket, s3_key, hasContractBeenSigned)
+    ($1, $2, $3, false);
+    `, [
+      req.body.client_id,
+      'pretslonboardingappbucket',
+      file.path
+    ]).promise();
+   }
+
+  res.sendStatus(201);
+})
--- src/components/AdminIndividualStore/AdminIndividualStore.js
+++ src/components/AdminIndividualStore/AdminIndividualStore.js
@@ -82,41 +82,55 @@ class AdminIndividualStore extends Component {
 
 
 
-
+    MyUploader = () =>{
     // specify upload params and url for your files
-    getUploadParams = async ({ meta }) => { 
-    const res = await axios.post('/upload-url');
-    console.log("POST /upload-url", res.data);
+        const getUploadParams = () => (
+            {
+            client_id: this.state.user_id,
+            url: '/upload'
+        });
+        // const getUploadParams = async ({ meta }) => { 
+        // const res = await axios.post('/upload');
+        // console.log("POST /upload", res.data);
 
-    const params = { 
-        fields: res.data.fields, 
-        meta: { 
-          fileUrl: `${res.data.url}/${res.data.fields.key}`
-        }, 
-        url: res.data.url 
-      };
-      console.log('params', params);
-  
-      // ENABLE CORS on your bucket
-      /**
-        <?xml version="1.0" encoding="UTF-8"?>
-        <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-        <CORSRule>
-            <AllowedOrigin>http://localhost:3000</AllowedOrigin>
-            <AllowedMethod>POST</AllowedMethod>
-            <AllowedHeader>*</AllowedHeader>
-        </CORSRule>
-        </CORSConfiguration>
-       */
-  
-      return params;
+        // const params = { 
+        //     fields: res.data.fields, 
+        //     meta: { 
+        //     fileUrl: `${res.data.url}/${res.data.fields.key}`
+        //     }, 
+        //     url: res.data.url 
+        // };
+        // console.log('params', params);
+    
+        // ENABLE CORS on your bucket
+        /**
+            <?xml version="1.0" encoding="UTF-8"?>
+            <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+            <CORSRule>
+                <AllowedOrigin>http://localhost:3000</AllowedOrigin>
+                <AllowedMethod>POST</AllowedMethod>
+                <AllowedHeader>*</AllowedHeader>
+            </CORSRule>
+            </CORSConfiguration>
+        */
+    
+        // return params;
+        // }
+
+
+        // receives array of files that are done uploading when submit button is clicked
+        const handleSubmit = (files, allFiles) => {
+       
+        allFiles.forEach(f => f.remove());
     }
 
-     // receives array of files that are done uploading when submit button is clicked
-    handleSubmit = (files, allFiles) => {
-    console.log(files.map(f => f.meta))
-    allFiles.forEach(f => f.remove())
-  }
+    return (
+        <Dropzone
+            getUploadParams={getUploadParams}
+            onSubmit = {handleSubmit}
+            />
+    )
+    }
 
     handleClose = () => this.setState({ modalOpen: false })
 
@@ -250,10 +264,11 @@ class AdminIndividualStore extends Component {
                 </Form>
                 <div className="contractUploader">
                     <h3>Upload Client Contract</h3>
-                    <Dropzone
+                    {/* <Dropzone
                         getUploadParams={this.getUploadParams}
                         onSubmit={this.handleSubmit}
-                    />
+                    /> */}
+                    <this.MyUploader/>
                 </div>
 
             </div>
