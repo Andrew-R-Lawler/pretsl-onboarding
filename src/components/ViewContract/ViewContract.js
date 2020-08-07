d10bcaf7d233af89bc6fc342f2d81145c333cf35
Andrew Lawler <andrewlawler@Andrews-MacBook-Air.local>
2020-08-07 10:14:52 -0500

    updates

--- src/components/ViewContract/ViewContract.js
+++ src/components/ViewContract/ViewContract.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Button, Modal, Header} from 'semantic-ui-react';
+import { Image, Button, Modal, Header} from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import { Document } from 'react-pdf';
 import FileViewer from 'react-file-viewer';
@@ -15,12 +15,7 @@ class ViewContract extends Component {
                 <Modal trigger={ <Button>View Contract</Button> }>
                     <Header icon="pen square" content="Register New Customer"/>
                         <Modal.Content>
-                            <Header content="PDF of contract should load here instead of spinning blue circle. It is pretty though, I'll give it that!"/>
-                                {/* <FileViewer
-                                    filePath='./pretsl_merchant_contract_agreement.jpg'
-                                    fileType='jpeg'
-                                /> */}
-                        <Document file='https://drive.google.com/file/d/1fHg6XyjQqJQPdrfeU0UB9_B4kA_0jSmy/view?usp=sharing' />
+                            {/* <Image */}
                         </Modal.Content>
                 </Modal>
             </div>
