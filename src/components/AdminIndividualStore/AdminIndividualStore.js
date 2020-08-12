c11627192f9b2d5744798f1f3f4fda34c840db4c
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-08-12 15:37:59 -0500

    update on code comments

--- src/components/AdminIndividualStore/AdminIndividualStore.js
+++ src/components/AdminIndividualStore/AdminIndividualStore.js
@@ -83,7 +83,6 @@ class AdminIndividualStore extends Component {
 
     handleInputChangeFor = (propertyName) => (event) => {
         this.setState({
-            // store_id: this.props.reduxState.locationsReducer[0].store_id,
             [propertyName]: event.target.value,
         });
     };
@@ -201,16 +200,6 @@ class AdminIndividualStore extends Component {
     MyUploader = () =>{
     // specify upload params and url for your files
         const getUploadParams = () => (
-            // try{{
-            //     url: '/upload'
-            // }
-            // }catch(err);
-            // try{{
-
-            //     url: '/upload',
-            //     client_id: this.state.user_id
-                
-            // }
 
             {
                 url:'/upload',
@@ -221,33 +210,7 @@ class AdminIndividualStore extends Component {
                 
             }
         );
-        // const getUploadParams = async ({ meta }) => { 
-        // const res = await axios.post('/upload');
-        // console.log("POST /upload", res.data);
 
-        // const params = { 
-        //     fields: res.data.fields, 
-        //     meta: { 
-        //     fileUrl: `${res.data.url}/${res.data.fields.key}`
-        //     }, 
-        //     url: res.data.url 
-        // };
-        // console.log('params', params);
-    
-        // ENABLE CORS on your bucket
-        /**
-            <?xml version="1.0" encoding="UTF-8"?>
-            <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-            <CORSRule>
-                <AllowedOrigin>http://localhost:3000</AllowedOrigin>
-                <AllowedMethod>POST</AllowedMethod>
-                <AllowedHeader>*</AllowedHeader>
-            </CORSRule>
-            </CORSConfiguration>
-        */
-    
-        // return params;
-        // }
 
 
         // receives array of files that are done uploading when submit button is clicked
